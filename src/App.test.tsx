7a8d3cfae289411877770135369e4e3dc17fac5d
Defi Boy <defiboy@machinalabs.dev>
2020-09-23 06:07:54 +0100

    added snapshot test

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import App from './App'
+
+describe("App", () => {
+  test('app snapshot', () => {
+    const { container } = render(<App />)
+
+
+    expect(container).toMatchSnapshot()
+  })
+})
